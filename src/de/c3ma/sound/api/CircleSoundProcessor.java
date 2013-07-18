94dbb5075b459279c533c9b7590fbf8d03955c53
0110 <github@buxs.org>
2013-07-18 18:44:21 +0200

    There is movement in the Circle

---
+++ src/de/c3ma/sound/api/CircleSoundProcessor.java
@@ -0,0 +1,149 @@
+package de.c3ma.sound.api;
+
+import java.awt.Color;
+import java.io.IOException;
+
+import javax.sound.sampled.SourceDataLine;
+
+import kjdss.KJDigitalSignalProcessor;
+import kjdss.KJDigitalSignalSynchronizer;
+import de.c3ma.animation.RainbowEllipse;
+import de.c3ma.fullcircle.RawClient;
+import de.c3ma.proto.fctypes.Frame;
+import de.c3ma.proto.fctypes.FullcircleSerialize;
+import de.c3ma.proto.fctypes.InfoAnswer;
+import de.c3ma.proto.fctypes.Pixel;
+import de.c3ma.proto.fctypes.Start;
+import de.c3ma.proto.fctypes.Timeout;
+
+public class CircleSoundProcessor implements KJDigitalSignalProcessor {
+
+    public static final float DEFAULT_VU_METER_DECAY = 0.02f;
+
+    protected float vuDecay = DEFAULT_VU_METER_DECAY;
+    float[] oldVolume;
+
+    private int width = 0;
+    private int height = 0;
+    private RawClient rc;
+
+    private int xmittel;
+
+    private int ymittel;
+
+    private int r;
+
+    public CircleSoundProcessor(String addr) throws Exception {
+        rc = new RawClient(addr);
+        rc.requestInformation();
+
+        while (true) {
+            Thread.sleep(10);
+            FullcircleSerialize got = rc.readNetwork();
+            if (got != null) {
+                System.out.println(got);
+                if (got instanceof InfoAnswer) {
+                    /*
+                     * Extract the expected resolution and use these values for
+                     * the request
+                     */
+                    InfoAnswer ia = (InfoAnswer) got;
+
+                    height = ia.getHeight();
+                    width = ia.getWidth();
+                    
+                    xmittel = (int) Math.ceil(width / 2);
+                    ymittel = (int) Math.ceil(height / 2);
+                    r = Math.min(xmittel, ymittel)  - 1;
+
+                    /*
+                     * when we got the resolution of the map, in this example we
+                     * now want to start to send something
+                     */
+                    rc.requestStart("java", 1, ia.getMeta());
+                } else if (got instanceof Start) {
+                    System.out.println("We have a GOOO send some data!");
+                    break;
+                } else if (got instanceof Timeout) {
+                    System.out.println("Too slow, so we close the session");
+                    rc.close();
+                    System.exit(1);
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void initialize(int arg0, SourceDataLine arg1) {
+        oldVolume = new float[arg1.getFormat().getChannels()];
+    }
+
+    @Override
+    public void process(KJDigitalSignalSynchronizer.Context arg0) {
+
+        float[][] pChannels = arg0.getDataNormalized();
+
+        float pFrrh = arg0.getFrameRatioHint();
+
+        float[] wVolume = new float[pChannels.length];
+
+        float wSadfrr = (vuDecay * pFrrh);
+
+        for (int a = 0; a < pChannels.length; a++) {
+
+            System.out.println("Channel " + a + ":" + pChannels[a].length);
+
+            for (int b = 0; b < pChannels[a].length; b++) {
+
+                float wAmp = Math.abs(pChannels[a][b]);
+
+                if (wAmp > wVolume[a]) {
+                    wVolume[a] = wAmp;
+                }
+
+            }
+
+            if (wVolume[a] >= (oldVolume[a] - wSadfrr)) {
+                oldVolume[a] = wVolume[a];
+            } else {
+
+                oldVolume[a] -= wSadfrr;
+
+                if (oldVolume[a] < 0) {
+                    oldVolume[a] = 0;
+                }
+
+            }
+
+            // System.out.println(a +" - "+ oldVolume[ a ] * ((float)
+            // (height*255) - 32) );
+
+        }
+
+        final Frame f = new Frame();
+
+        int a = (int) (oldVolume[0] * ((float) (height * 255) - 32));
+        int b = (int) (oldVolume[1] * ((float) (height * 255) - 32));
+
+        int louder = Math.max(a, b);
+        
+        new RainbowEllipse(this.xmittel, this.ymittel, this.r, this.r) {
+
+            @Override
+            protected void drawPixel(int x, int y, Color c) {
+                f.add(new Pixel(x, y, c));                        
+            }
+            
+        }.drawEllipse(louder);
+
+        try {
+            rc.sendFrame(f);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/de/c3ma/sound/client/CircleSound.java
@@ -0,0 +1,74 @@
+package de.c3ma.sound.client;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.SourceDataLine;
+
+import kjdss.KJDigitalSignalSynchronizer;
+import de.c3ma.sound.api.CircleSoundProcessor;
+
+class CircleSound {
+	private static final int READ_BUFFER_SIZE = 1024 * 4;
+
+	public static void main(String args[]) throws Exception {
+		
+	    if (args.length != 2) {
+	        System.out.println("Usage: <IP of Wall> <MP3 to visualize>");
+	        return;
+	    }
+	    
+		String adress = args[0];
+		String filename = args[1]; /* eg.g "/home/bene/Downloads/01_sad_robot.mp3"; */
+		KJDigitalSignalSynchronizer dss;
+		
+		   File file = new File(filename);
+		    AudioInputStream in= AudioSystem.getAudioInputStream(file);
+		    AudioInputStream wAs = null;
+		    AudioFormat baseFormat = in.getFormat();
+
+		    AudioFormat decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+                    baseFormat.getSampleRate(),
+                    16,
+                    baseFormat.getChannels(),
+                    baseFormat.getChannels() * 2,
+                    baseFormat.getSampleRate(),
+                    false);
+		    wAs = AudioSystem.getAudioInputStream(decodedFormat, in);
+		    
+		    CircleSoundProcessor sp = new CircleSoundProcessor(adress);
+		    
+		    dss = new KJDigitalSignalSynchronizer();
+			
+			dss.add( sp );
+		    
+			SourceDataLine wSdl = AudioSystem.getSourceDataLine( wAs.getFormat() );
+
+        	// -- Open the source data line and start it.
+        	wSdl.open();
+        	wSdl.start();
+
+        	// -- Have the DSS monitor the source data line.
+            dss.start( wSdl );
+        	
+            // -- Allocate a read buffer.
+        	byte[] wRb = new byte[ READ_BUFFER_SIZE ];
+        	int    wRs = 0;
+        	
+        	// -- Read from WAV file and write to DSS (and the monitored source data line)
+        	while( ( wRs = wAs.read( wRb ) ) != -1 ) {
+        		dss.writeAudioData( wRb, 0, wRs );
+        	}
+
+        	// -- EOF, stop monitoring source data line.
+        	dss.stop();
+        	
+        	// -- Stop and close the source data line.
+        	wSdl.stop();
+        	wSdl.close();
+		
+	}
+
+}
